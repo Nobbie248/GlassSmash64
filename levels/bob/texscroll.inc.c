6c50e7c2827416b0710ce517ccd13f5332af3730
Gitboy-2000 <romansuperduper@gmail.com>
2025-02-10 18:14:36 +1100

    opti

--- levels/bob/texscroll.inc.c
+++ levels/bob/texscroll.inc.c
@@ -63,12 +63,33 @@ void scroll_bob_dl_ground_001_mesh_layer_5_vtx_2() {
 
 void scroll_bob_dl_zsplash_mesh_layer_5_vtx_0() {
 	int i = 0;
-	int count = 24;
+	int count = 12;
 	int width = 64 * 0x20;
 
 	static int currentX = 0;
 	int deltaX;
 	Vtx *vertices = segmented_to_virtual(bob_dl_zsplash_mesh_layer_5_vtx_0);
+
+	deltaX = (int)(0.30000001192092896 * 0x20) % width;
+
+	if (absi(currentX) > width) {
+		deltaX -= (int)(absi(currentX) / width) * width * signum_positive(deltaX);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[0] += deltaX;
+	}
+	currentX += deltaX;
+}
+
+void scroll_bob_dl_zsplash_001_mesh_layer_5_vtx_0() {
+	int i = 0;
+	int count = 12;
+	int width = 64 * 0x20;
+
+	static int currentX = 0;
+	int deltaX;
+	Vtx *vertices = segmented_to_virtual(bob_dl_zsplash_001_mesh_layer_5_vtx_0);
 
 	deltaX = (int)(0.30000001192092896 * 0x20) % width;
 
@@ -96,5 +117,6 @@ void scroll_bob() {
 	scroll_bob_dl_ground_001_mesh_layer_5_vtx_1();
 	scroll_bob_dl_ground_001_mesh_layer_5_vtx_2();
 	scroll_bob_dl_zsplash_mesh_layer_5_vtx_0();
+	scroll_bob_dl_zsplash_001_mesh_layer_5_vtx_0();
 	scroll_gfx_bob_dl_ground_001_mesh_layer_5();
 };
