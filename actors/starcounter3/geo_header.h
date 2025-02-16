b557f4f8d0abf3f8f43a1a9b205fb00069aab563
Gitboy-2000 <romansuperduper@gmail.com>
2025-02-16 17:06:53 +1100

    hubi

--- actors/starcounter3/geo_header.h
+++
@@ -1,9 +0,0 @@
-extern const GeoLayout starcounter3_geo[];
-extern Lights1 starcounter3_f3dlite_material_003_lights;
-extern u8 starcounter3_MARIOKART64_069367CC_0_2_all_ci4[];
-extern u8 starcounter3_MARIOKART64_069367CC_0_2_all_pal_rgba16[];
-extern Vtx starcounter3_counter_mesh_layer_1_vtx_0[4];
-extern Gfx starcounter3_counter_mesh_layer_1_tri_0[];
-extern Gfx mat_starcounter3_f3dlite_material_003[];
-extern Gfx mat_revert_starcounter3_f3dlite_material_003[];
-extern Gfx starcounter3_counter_mesh_layer_1[];
